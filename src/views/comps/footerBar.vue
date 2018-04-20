5389416bb9037fdfafab2bad6fdfe7d07b36bf91
minhill <lrrq369@hotmail.com>
2018-04-20 11:55:44 +0800

    修改成copyleft

--- src/views/comps/footerBar.vue
+++ src/views/comps/footerBar.vue
@@ -4,7 +4,7 @@
   <span><a href="https://github.com/hanchyhill/oceanMonitor" target="_blank">代码仓库</a> | </span>
   <span>BUG与建议提交: <a href="http://bug.gdmo.gq/" target="_blank">内网</a>、
   <a href="https://github.com/hanchyhill/oceanMonitor/issues" target="_blank">外网</a></span>
-  <span> © 广东省气象台 - 南海海洋气象预报中心</span>
+  <div class="copy-left">© </div> <span> 广东省气象台 - 南海海洋气象预报中心</span>
 </div>
 </template>
 
@@ -37,6 +37,11 @@
 }
 .footer a{
   color:#9fe0f6;
+}
+
+.copy-left{
+  transform:rotate(180deg);
+  display: inline-block;
 }
 </style>
 
