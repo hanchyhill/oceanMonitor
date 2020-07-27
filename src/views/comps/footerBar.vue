8544b353b1a909832540c08cd780e2a73c06646c
minhill <lrrq369@gmail.com>
2020-07-27 18:58:58 +0800

    remove: GDMO and github url

--- src/views/comps/footerBar.vue
+++ src/views/comps/footerBar.vue
@@ -1,10 +1,11 @@
 <template>
 <div class='footer'>
-  <span><a>站点地图</a> | </span>
-  <span><a href="https://github.com/hanchyhill/oceanMonitor" target="_blank">代码仓库</a> | </span>
-  <span>BUG与建议提交: <a href="http://bug.gdmo.gq/" target="_blank">内网</a>、
-  <a href="https://github.com/hanchyhill/oceanMonitor/issues" target="_blank">外网</a></span>
-  <div class="copy-left">© </div> <span> 广东省气象台 - 南海海洋气象预报中心</span>
+  <!-- <span><a>站点地图</a> | </span> -->
+  <!-- <span><a href="https://github.com/hanchyhill/oceanMonitor" target="_blank">代码仓库</a> | </span> -->
+  <span>BUG与建议提交: <a href="http://bug.gdmo.gq/" target="_blank">内网</a>
+  <!-- 、<a href="https://github.com/hanchyhill/oceanMonitor/issues" target="_blank">外网</a> -->
+  </span>
+  <div class="copy-left">© </div> <span> GDMO - 南海海洋气象预报中心</span>
 </div>
 </template>
 
