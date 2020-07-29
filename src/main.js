c1e36b4ed419f2718c0e694943460e7bfb675d02
minhill <lrrq369@gmail.com>
2020-07-29 11:34:53 +0800

    fix import error of polyfill.js

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import Vuex from 'vuex';
 import Util from './libs/util';
 import App from './app.vue';
 import {registerSW,testPush,subscribePush,isSubscribe} from './libs/push.js';
-import polify from './libs/polify.js';
+import polify from './libs/polyfill.js';
 import 'iview/dist/styles/iview.css';
 // import urlBase64ToUint8Array from './libs/base64util';
 // import runtime from 'serviceworker-webpack-plugin/lib/runtime';
