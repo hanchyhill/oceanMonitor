fe9aab3d07751d3d985a1b84a326283678316773
minhill <lrrq369@gmail.com>
2022-02-15 10:52:22 +0800

    Update footerBar.vue

--- src/views/comps/footerBar.vue
+++ src/views/comps/footerBar.vue
@@ -1,10 +1,10 @@
 <template>
 <div class='footer'>
-  <!-- <span><a>站点地图</a> | </span> -->
-  <!-- <span><a href="https://github.com/hanchyhill/oceanMonitor" target="_blank">代码仓库</a> | </span> -->
-  <span>BUG与建议提交: <a href="http://bug.gdmo.gq/" target="_blank">内网</a>
+  <span><a>站点地图</a> | </span>
+  <!--<span><a href="https://github.com/hanchyhill/oceanMonitor" target="_blank">代码仓库</a> | </span>
+   <span>备案号: <a href="https://beian.miit.gov.cn/" target="_blank">粤ICP备20030351号-1</a> </span>-->
   <!-- 、<a href="https://github.com/hanchyhill/oceanMonitor/issues" target="_blank">外网</a> -->
-  </span>
+  
   <div class="copy-left">© </div> <span> GDMO - 南海海洋气象预报中心</span>
 </div>
 </template>
